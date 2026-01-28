262754675c6f57cf2d5d6eaaa80189d8d8ecfaaa
Estherngaire <essymso59@gmail.com>
2026-01-28 08:07:24 +0300

    alignment on NewsLetter.jsx

--- src/Components/sections/NewsLetter.jsx
+++ src/Components/sections/NewsLetter.jsx
@@ -18,16 +18,16 @@ const NewsletterSection = () => {
       <div className='ml-10 w-1/2'>
         <ResponsiveImage src={Accessories} alt="Accessory" className='w-full max-h-[420px] object-cover rounded-xl' />
       </div>
-      <div className='mt-30 w-1/2 flex flex-row md:flex-col items-center text-center px-4'>
+      <div className='mt-30 w-1/2 flex flex-col items-center text-center px-4'>
         <h3 className="text-3xl font-bold">Get more discount Off your order</h3>
         <p className="mb-8 font-semibold">Join our mailing list</p>
-        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
           <input
             type="email"
             placeholder="Your email address"
             value={email}
             onChange={e => setEmail(e.target.value)}
-            className="bg-neutralDark w-1/2 md:w-full px-4 py-2"
+            className="bg-neutralDark px-4 py-2 rounded w-full sm:w-auto text-neutralLight"
           />
           <Button type="submit" className="w-full sm:w-auto">Shop Now</Button>
         </form>
